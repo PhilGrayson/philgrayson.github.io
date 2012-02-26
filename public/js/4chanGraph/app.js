f2b888b02c307a9c6a6a22e134fb1ed22de8d431
Phil Grayson <phil@philgrayson.com>
2012-02-26 20:42:11 +0000

    Add ajax and highcharts graph result

--- public/js/4chanGraph/app.js
+++ public/js/4chanGraph/app.js
@@ -1,14 +1,70 @@
-var options = {
-	chart : {
-		renderTo: 'graphContainer',
-		defaultSeriesType: 'bar',
-	},
-	series: [{
-		name: 'Jane',
-		data: [1, 0, 4]
-	}]
-};
-
 $(document).ready(function() {
-	chart = new Highcharts.Chart(options);
+	var drawGraph = function(result) {
+		var options = {
+			chart : {
+				renderTo: 'graphContainer',
+				defaultSeriesType: 'spline',
+			},
+			yAxis: {
+				title: {
+					text: 'Number of posts'
+				}
+			},
+			xAxis: {
+				title: {
+					text: 'Date'
+				},
+				type: 'datetime'
+			},
+			series: []
+		};
+
+		if (!$.isEmptyObject(result)) {
+			$.each(result.boards, function(index, board) {
+				var series = result.data[board].map(function(point) {
+					return [Date.parse(point.date), parseInt(point.count)];
+				});
+
+				options.series.push({name: board, data: series});
+			});
+		}
+		
+		new Highcharts.Chart(options);
+	};
+
+
+	$('.boardToggle').click(function() {
+		$(this).toggleClass('active');
+		return false;
+	});
+
+	$('#graph').click(function() {
+		var data = {};
+
+		now  = new Date();
+		then = new Date();
+		then.setDate(then.getDate() - 8);
+
+		data.from = then.toJSON();
+		data.to =  now.toJSON();
+
+		var boards = [];
+		$('.boardToggle.active').each(function(index, elem) {
+			boards.push(elem.id);
+		});
+
+		if (boards.length > 0) {
+			data.boards = boards.reduce(function(prev, curr, index) {
+				return prev + ',' + curr;
+			});
+
+			$.ajax({
+				url: '/4chan-graph',
+				data: data,
+				success: drawGraph,
+				dataType: 'json'
+			});
+		}
+	});
+
 });
