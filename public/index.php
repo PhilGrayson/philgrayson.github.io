118fcb3b734b5a574ec19462be6da6684118383e
Phil Grayson <phil@philgrayson.com>
2012-04-21 16:52:39 +0100

    Add blog routes

--- public/index.php
+++ public/index.php
@@ -15,9 +15,9 @@
 		'db.options' => array(
 			  'driver' => 'pdo_mysql',
 			  'dbname' => '4changraph',
-			    'user' => 'root',
-			'password' => '',
-			    'host' => 'localhost',
+			    'user' => 'dev-user',
+			'password' => 'user-dev',
+			    'host' => 'philgrayson.com',
 		),
 		'db.dbal.class_path'   => root_dir . 'Application/vendor/doctrine-dbal/lib',
 		'db.common.class_path' => root_dir . 'Application/vendor/doctrine-common/lib',
@@ -26,6 +26,17 @@
 	// Add Application namespace to autoloader
 	$app['autoloader']->registerNamespace('Application', root_dir);
 	
+	$app->get('/', function() use ($app) {
+		$controller = new Application\Controller\blog($app);
+		return $controller->index();
+	});
+
+	$app->get('/{year}/{month}/{name}', function($year, $month, $name) use ($app) {
+		$controller = new Application\Controller\blog($app);
+		return $controller->show($year, $month, $name);
+	})->assert('year', '\d{4}')
+	  ->assert('month', '\d{2}');
+
 	// Misc tools routes
 	$app->get('/misc-tools', function() use ($app) {
 		$controller = new Application\Controller\miscTools($app);
