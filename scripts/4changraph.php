675879e04f01a9a0332a7c79fa4fe1bbac88f92d
Phil Grayson <phil@philgrayson.com>
2012-04-27 23:41:07 +0100

    Clean up namespace paths

--- scripts/4changraph.php
+++ scripts/4changraph.php
@@ -4,9 +4,8 @@
   $app = require 'index.php';
 
   use Symfony\Component\Console\Application;
-  use Symfony\Component\Console\Input\InputInterface;
-  use Symfony\Component\Console\Input\InputArgument;
   use Symfony\Component\Console\Input\InputOption;
+  use Symfony\Component\Console\Input\InputInterface;
   use Symfony\Component\Console\Output\OutputInterface;
 
   $console = new Application();
