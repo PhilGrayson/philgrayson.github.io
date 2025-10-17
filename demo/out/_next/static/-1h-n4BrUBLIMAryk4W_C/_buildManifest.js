c9d5443e83a48cd4d3208dca3363a3d8c247e2e0
Phil Grayson <phil@philgrayson.com>
2025-10-18 00:05:36 +0100

    latest

---
+++ demo/out/_next/static/-1h-n4BrUBLIMAryk4W_C/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/23dd2123482dc330.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ demo/out/_next/static/chunks/890957ac2a303a10.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/46f625a2f0144f57.js",
+  "static/chunks/turbopack-c9addbd48fbedaa5.js"
+])
